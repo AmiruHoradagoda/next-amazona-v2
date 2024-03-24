78bcd75a0e8ca2848306f29aae3ab351119251ad
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-03-24 15:02:15 +0530

    01-Create-Next-App-Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">  
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>Copyright @ 2023 - All right reserved bt Next Amorzon V2</p>
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ components/header/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Next Amazona V2
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">
+                Cart
+              </Link>
+              <Link href="/signin" className="btn btn-ghost rounded-btn">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
